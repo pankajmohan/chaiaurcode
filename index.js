a30ec946f49d724c50e708da08349642154b3509
Pankaj Mohan <pankaj.mohan282@gmail.com>
2025-05-15 15:57:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config(); 
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/chai',(req, res)=>{
+    res.send("<h2>Chai Aur Code</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
